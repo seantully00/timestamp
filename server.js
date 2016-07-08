d4bb0d07d737a8465bf1b11a138df4a78822cd26
Sean <seantully00@hotmail.com>
2016-07-08 02:11:57 +0000

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('https://sttimestamp.herokuapp.com/*', function(res, req) {
+    
+})
+
+
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('App listening on port 8080!');
 });
