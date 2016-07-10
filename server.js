fe97e4a7a9c57f9c4703d23f7be4c1097ad753a5
Sean <seantully00@hotmail.com>
2016-07-10 00:58:03 +0000

    latest commit

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
+var url = require('url');
+var http = require('http');
+var unix, natural;
+var moment = require('moment');
+
+  
 
 app.get('https://sttimestamp.herokuapp.com/*', function(res, req) {
-    
+    var urlObj = url.parse(req.url, true),
+      pathname = urlObj.pathname;
+      var date = new Date(pathname);
+      if (isNaN(Number(pathname)) && date != 'Invalid Date') {
+          natural = pathname;
+          unix = Date.parse(pathname);
+      } else if(!isNaN(Number(pathname))){
+		natural = moment.unix(Number(pathname)).format("MMMM DD, YYYY");
+		unix = pathname;
+	}
+	else { natural = null; unix = null; } 
+	let data = {"unix": unix, "natural": natural};
+	res.write(JSON.stringify(data));
+	res.end();
 })
 
 
