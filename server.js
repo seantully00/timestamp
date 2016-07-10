9a07de9addcc5a9dcd895f3a35bbd426b282f43f
Sean <seantully00@hotmail.com>
2016-07-10 01:43:18 +0000

    latest commit

--- server.js
+++ server.js
@@ -4,30 +4,28 @@ var express = require('express');
 var app = express();
 var url = require('url');
 var http = require('http');
-var unix, natural, time;
+var unix, natural, data;
 var moment = require('moment');
 var port = process.env.PORT || 8080;  
 
   
 
 app.get('/:time', function(req, res) {
-    var timestamp = moment(req.params.time, 'MMMM DD, YYYY', true);
-        if (!timestamp.isValid()) {
-            time = moment.unix(req.params.timestamp);
-        }
-         
-        if (!time.isValid()) {
-    res.json({
-      'unix': null,
-      'natural': null
-    });
-  }
-  
-  res.json({
-    'unix': time.format('X'),
-    'natural': time.format('MMMM DD, YYYY')
-  });
-        });
+    var urlObj = url.parse(req.url, true),
+      pathname = urlObj.pathname;
+      var date = new Date(pathname);
+      if (isNaN(Number(pathname)) && date != 'Invalid Date') {
+          natural = pathname;
+          unix = Date.parse(pathname);
+      } else if(!isNaN(Number(pathname))){
+		natural = moment.unix(Number(pathname)).format("MMMM DD, YYYY");
+		unix = pathname;
+	}
+	else { natural = null; unix = null; } 
+	data = {"unix": unix, "natural": natural};
+	res.write(JSON.stringify(data));
+	res.end();
+})
 
 
 
